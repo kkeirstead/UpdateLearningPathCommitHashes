b0533f033ba96daec29e930b0c9edb764327e78c
kkeirstead <kkeirstead@microsoft.com>
2024-01-23 12:21:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require('@actions/core');
+const fs = require('fs');
+const actionUtils = require('../action-utils.js');
+
+const main = async () => {
+  try {
+    //const [core] = await actionUtils.installAndRequirePackages("@actions/core");
+    const learningPathDirectory = core.getInput('learningPathsDirectory', { required: true });
+    const oldHash = core.getInput('oldHash', { required: true });
+    const newHash = core.getInput('newHash', { required: true });
+    
+    if (changedFilePaths === null || changedFilePaths.trim() === "") { return }
+
+    // Scan each file in the learningPaths directory
+    fs.readdir(learningPathDirectory, (err, files) => {
+      files.forEach(learningPathFile => {
+
+        try {
+          const learningPathFileContent = fs.readFileSync(learningPathDirectory + "/" + learningPathFile, "utf8")
+
+          // Replace all instances of the oldHash with the newHash
+          const headLearningPathFileContentStr = learningPathFileContent.replace(new RegExp(oldHash, 'g'), newHash);
+
+          // Use actionUtils to write the updated learning path file to the repo
+          actionUtils.writeFile(learningPathDirectory + "/" + learningPathFile, headLearningPathFileContentStr);
+
+        } catch (error) {
+          console.log("Error: " + error)
+          console.log("Could not find learning path file: " + learningPathFile)
+        }
+      });
+    });
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+// Call the main function to run the action
+main();
